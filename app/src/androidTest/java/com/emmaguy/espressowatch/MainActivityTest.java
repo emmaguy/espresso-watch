a9794d71ccc75e2622433bf7d9433e097b19cb79
Emma Guy <emmag87@gmail.com>
2015-10-10 21:42:44 +0100

    Update test method name to make it clearer, improve formatting

--- app/src/androidTest/java/com/emmaguy/espressowatch/MainActivityTest.java
+++ app/src/androidTest/java/com/emmaguy/espressowatch/MainActivityTest.java
@@ -14,12 +14,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+    @Rule public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
-
-    @Test
-    public void buttonShouldUpdateText() {
+    @Test public void textShouldSayHelloSquareWorld() {
         onView(withId(R.id.text)).check(matches(withText(R.string.hello_square)));
     }
 }
