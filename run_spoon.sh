54872a54d9c35ff8dbe3b22d734d8ad09c5d7226
Emma Guy <emmag87@gmail.com>
2015-10-13 16:28:40 +0100

    Update apk names to use 'dev' flavor

--- run_spoon.sh
+++ run_spoon.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-java -jar spoon-runner-1.2.0-jar-with-dependencies.jar --apk app/build/outputs/apk/app-debug.apk --test-apk app/build/outputs/apk/app-debug-androidTest-unaligned.apk
+java -jar spoon-runner-1.2.0-jar-with-dependencies.jar --apk app/build/outputs/apk/app-dev-debug.apk --test-apk app/build/outputs/apk/app-dev-debug-androidTest-unaligned.apk
 
 echo "Done! Opening output"
 open spoon-output/index.html
